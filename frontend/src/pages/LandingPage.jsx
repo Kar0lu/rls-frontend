0f83d2953b8e2ffcbc84e96fd935353a60994602
Kar0lu <karolugod@gmail.com>
2025-01-18 21:49:27 +0100

    fix: cleaned up landing page structure and got rid of unnecesary components

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import { Typography, Box } from '@mui/material';
-import LoginForm from '../components/LoginForm';
+import React, {useContext, useState} from 'react';
+import { TextField, Button, Typography, Box, Card, CardContent, Tooltip } from '@mui/material';
+import AuthContext from '../context/AuthContext';
 
 const LandingPage = () => {
+  let {loginUser} = useContext(AuthContext)
+
+  const [formValues, setFormValues] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((prevValues) => ({
+        ...prevValues,
+        [name]: value,
+    }));
+  };
+
+  const handleLogin = () => {
+    loginUser(formValues.username, formValues.password)
+  }
+
   return (
     <Box
       sx={{
@@ -11,10 +30,10 @@ const LandingPage = () => {
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        background: 'linear-gradient(135deg, #1e1e2f, #1c1c25)',
+        backgroundColor: 'background.default',
       }}
     >
-      <Box maxWidth="lg" sx={{ textAlign: 'center', mb: 5 }}>
+      <Box sx={{ maxWidth: 'lg', textAlign: 'center', mb: 5 }}>
         <Typography variant="h1" gutterBottom>
           Remote Lab System
         </Typography>
@@ -22,7 +41,53 @@ const LandingPage = () => {
           Kontroluj zdalne zasoby laboratoryjne, zarządzaj eksperymentami i uzyskuj dostęp do danych na jednej zintegrowanej platformie.
         </Typography>
       </Box>
-      <LoginForm />
+      <Card sx={{ backgroundColor: 'background.paper', boxShadow: 3, minWidth: 400 }}>
+        <CardContent>
+          <Box
+            component="form"
+            sx={{
+              mt: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 2,
+            }}
+          >
+            <TextField
+              label="Nazwa użytkownika"
+              type="text"
+              variant="outlined"
+              fullWidth
+              name="username"
+              onChange={handleInputChange}
+            />
+            <TextField
+              label="Hasło"
+              type="password"
+              variant="outlined"
+              fullWidth
+              name="password"
+              onChange={handleInputChange}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{ mt: 2 }}
+              onClick={handleLogin}
+            >
+              Zaloguj
+            </Button>
+            <Tooltip title="Skontaktuj się z kierownikiem swojego laboratorium" arrow enterDelay={500} leaveDelay={200}>
+              <Typography
+                variant="caption"
+                sx={{ cursor: 'pointer', color: 'secondary.main', textAlign: 'center' }}
+              >
+                Skąd wziąć hasło?
+              </Typography>
+            </Tooltip>
+          </Box>
+        </CardContent>
+      </Card>
     </Box>
   );
 }
