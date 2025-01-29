5393dc67ca18f8f1d7ab93e97a6237a10bd974c9
Kar0lu <karolugod@gmail.com>
2025-01-29 06:33:16 +0100

    fix: hotfix

--- frontend/src/modals/HarmonogramModal.jsx
+++ frontend/src/modals/HarmonogramModal.jsx
@@ -15,7 +15,7 @@ const HarmonogramModal = ({open, onClose, selectedDate, startHour, endHour, sele
     const fetchReservation = () => {
         showLoading()
         fetch(`http://127.0.0.1:8000/api/reservations/`, {
-            method: 'GET',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${authTokens.access}`,
